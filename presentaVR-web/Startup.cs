3eed2e55750f3d1f97087aaaae01e68cd64a19dc
Oleksandr Symonenko <osymonenko@amcbridge.com>
2020-02-22 16:03:41 +0200

    Add project files.

---
+++ presentaVR-web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(presentaVR.Startup))]
+namespace presentaVR
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
