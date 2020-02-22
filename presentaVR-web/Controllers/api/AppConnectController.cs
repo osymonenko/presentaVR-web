adf899e779ab82ccee1cf6c9e51c50a00799381c
Oleksandr Symonenko <osymonenko@amcbridge.com>
2020-02-22 17:14:31 +0200

    added configuration for API routes

---
+++ presentaVR-web/Controllers/api/AppConnectController.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System.Web.Http;
+
+namespace presentaVR.Controllers
+{
+    public class AppConnectController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult GetTestJson(string id)
+        {
+            var jsObj = new JsonObjTest() { Id = id };
+            return Ok(jsObj);
+        }
+
+        private class JsonObjTest
+        {
+            public string Id { get; set; }
+        }
+    }
+}
